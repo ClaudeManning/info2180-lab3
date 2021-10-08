b1255a90027809cfc612c049febbe5ad133997fb
Claude Manning <manningclaude0@gmail.com>
2021-10-08 18:31:55 -0500

    Tic Tac Toe Js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,75 +1,104 @@
-window.addEventListener('DOMContentLoaded', () =>{
-	/** Lab3 for Claude Manning **/
-	const board = document.getElementById("board");
-	const actualBoard = board.getElementsByTagName("div");
-  	let x= "X";
-  	const stat=document.querySelector('#status');
-  	const btnButton=document.getElementsByClassName("btn")[0];
-  	const rules=[[0,4,8],[0,3,6],[0,1,2],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
-  	let selected;
-  	let nexxt=true;
-	let arr=["","","","","","","","",""];
-	const clicksquare;
-	const index;
-	const gamewon= () => 'Congratulations ${x} is the winner ';
-	const gamedraw = () => 'None of you won';
-	const xplay = () => '${x} please play';
-	let sqaureselect;
-	let index2;
-	let square;
-	var d;
-	const disp;
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const winnings = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+	let x1 = "X";
+	let arr1 = ["", "", "", "", "", "", "", "", ""];
+	const winner = () => `Congratulations! ${x1} is the winner`;
+	const turn = () => ` Play ${x}`;
+	const stat = document.querySelector('#status');
+	let active = true;
 
-	function newboard() {
-		arr= ["", "", "", "", "", "", "", "", ""];
-
-			for(d=0; d<board.length; d++) {
-				actualBoard[d].innerHTML = "";
-				actualBoard[d].setAttribute("cellindex", d);
-				actualBoard[d].setAttribute("class", "square");
-				actualBoard[d].addEventListener('click', clicksquare);
-				actualBoard[d].addEventListener("mousemover", mouse);
-			}	  
-	}
-	function mousemover(){
-		document.getElementsByClassName("square").setAttribute("class", "hover");
-	}
-
-
-
-	function sq(square) {
-		 clicksquare = square.target;
-		 index= parseInt(clicksquare.getAttribute('cellindex'));
+	function sq(square1) {
+		const clicksquare = square1.target;
+		const index1 = parseInt(clickesquare.getAttribute('data-cell-index'));
 		
-		if (arr[index] !== "" || !nexxt) {
+		if (arr1[index1] !== "" || !active) {
         return;
 		}
-		inserted(clicksquare, index);
-		current();
+		opt(clicksquare, index1);
+		decider();
 	}
 
-	function insereted(sqaureselect, index2) {
-		 arr[index] = x;
-		 squareselect.innerHTML= x;
+
+	
+	function opt(sqselected, sqnum) {
+		arr1[sqnum] = x1;
+		sqselected.innerHTML = x1;
 
 	}
 	
-
 	function move() {
-		x = x === "X" ? "O" : "X";
-		disp.innerHTML = x();
+		x1 = x1 === "X" ? "O" : "X";
+		stat.innerHTML = turn();
 
 	}
+	
+	function decider() {
+		let ifWon = false;
+		for (var i = 0; i <= 7; i++) {
+        const condition = winnings[i];
+        let a = arr1[condition[0]];
+        let b = arr1[condition[1]];
+        let c = arr1[condition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            ifWon = true;
+            break
+			}
+		}
+		if (ifWon) {
+			stat.innerHTML = winner();
+			active= false;
+			return;
+		}
+		
 
-
-	function resetBoard() {
-		disp.innerHTML = x();
-		newboard();
-		activeGame = true;
+	   move();
 
 	}
 	
 
-	newboard();
-	btnButton.addEventListener('click', resetBoard);
-});
+		
+	function NewGame() {
+		stat.innerHTML = turn();
+		newBoard();
+		active = true;
+
+	}
+	
+	function hovering(){
+		document.getElementsByClassName("square").setAttribute("class", "hover");
+	}
+	
+
+
+	
+	function newBoard() {
+		arr1 = ["", "", "", "", "", "", "", "", ""];
+		let boardcreate = document.getElementById("board");
+		let boarddiv = boardcreate.getElementsByTagName("div");
+			for(var d=0; d<boarddiv.length; d++) {
+				boarddiv[d].innerHTML = "";
+				boarddiv[d].setAttribute("data-cell-index", d);
+				boarddiv[d].setAttribute("class", "square");
+				boarddiv[d].addEventListener('click', sq);
+				boarddiv[d].addEventListener("mouseover", hovering);
+			}	  
+	}
+
+	newBoard();
+	document.querySelector('.btn').addEventListener('click', NewGame);
+	
+	
+ });
